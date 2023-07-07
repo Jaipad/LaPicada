8c80d98a4555ab738b345410bde46640bb6e439b
Bastian Gavilan Guidotti <bastian.guidotti@gmail.com>
2023-07-07 14:39:35 -0400

    Cambio de nombre de logoblanco

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="foot">
-      <img className="logoB" src="/imagenes/logoBlanco.png" />
+      <img className="logoB" src="/imagenes/LogoBlanco.png" />
       <p className="copyright">&copy Copyright The Locales Squad - 2023</p>
     </footer>
   );
